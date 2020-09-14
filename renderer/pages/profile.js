773c08827724a5924a33034c876ebcc497107c2d
aidenaio <a@idena.io>
2020-09-14 17:06:35 +0500

    Revert "Auto report missing keywords (#425)"
    This reverts commit 15308ca191ea828e3612d4a4ba0ec4651bca6b54.

--- renderer/pages/profile.js
+++ renderer/pages/profile.js
@@ -202,7 +202,7 @@ export default function ProfilePage() {
                 <IconLink
                   href="/contacts/new-invite"
                   isDisabled={invitesCount === 0}
-                  icon={<Icon name="add-user" size={5} />}
+                  icon="add-user"
                 >
                   {t('Invite')}
                 </IconLink>
